8e91bd8d1b392e712f5b06ae045660f3b37c622b
JMatheus97 <matheus_love_vdk@outlook.com>
2022-12-26 22:43:35 -0300

    Classe Produto

---
+++ models/Produto.js
@@ -0,0 +1,30 @@
+const mongoose = require('../db/conn');
+const { Schema } = mongoose;
+
+const Produto = mongoose.model(
+    'Produto',
+    new Schema({
+        nome: {
+            type: String,
+            required: true,
+            allowNull: false,
+        },
+        valor: {
+            type: Number,
+            required: true,
+            allowNull: false,
+        },
+        quantidade: {
+            type: Number,
+            required: true,
+            allowNull: false
+        },
+        tipo: {
+            type: String,
+            required: true,
+            allowNull: false
+        }
+    })
+)
+
+module.exports = Produto;
