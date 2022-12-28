a438425d7ebf415ff11ac7cf2cc93761773df56a
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2022-12-27 22:48:45 -0300

    Criação classe Comanda

---
+++ models/Comanda.js
@@ -0,0 +1,37 @@
+const mongoose  = require("../db/conn");
+const { Schema } = mongoose;
+
+ const Comanda = mongoose.model(
+    'Comanda',
+    new Schema({
+        nomeCliente: {
+            type: String,
+            required: true,
+            allowNull: false,
+        },
+        valor: {
+            type: Number,
+            required: true,
+            allowNull: false,
+        },
+        mesa: {
+            type: Schema.Types.ObjectId,
+            ref:"Mesa",
+            required: true,
+            allowNull: false
+        },
+        usuario: { 
+            type: Schema.Types.ObjectId,
+            ref:"Usuario",
+            required: true,
+            allowNull: false,
+        },
+        produto: {
+            type: Schema.Types.ObjectId,
+            ref:"Produto",
+            required: true,
+        }
+    })
+ )
+
+ module.exports = Comanda;
