91130af798e3e7de3e5383c03e0d7781d0914aed
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2022-12-27 22:47:51 -0300

    Criação classe Estoque

---
+++ models/Estoque.js
@@ -0,0 +1,26 @@
+const mongoose = require("../db/conn");
+const { Schema } = mongoose;
+
+const Estoque = mongoose.model(
+    'Estoque',
+    new Schema({
+        produto: {
+            type: Schema.Types.ObjectId,
+            ref:"Produto",
+            required: true,
+            allowNull: false,
+        },
+        quantidade: {
+            type: Number,
+            required: true,
+            allowNull: false
+        },
+        tipo: {
+            type: String,
+            required: true,
+            allowNull: false
+        }
+    })
+)
+
+module.exports = Estoque;
