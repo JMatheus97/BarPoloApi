90710ff3a4a12cb1905d1f9541d7ce459ca12c65
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2022-12-31 01:11:41 -0300

    Criação metodo listar mesas

--- routes/MesaRoutes.js
+++ routes/MesaRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const MesaController = require('../controllers/MesaController');
 
+router.get('', MesaController.find);
 router.post('/new', MesaController.create);
 
 module.exports  = router;
