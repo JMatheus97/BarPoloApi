b3f8b7196dcee32790ffa3cbd41efb26e3dae9f7
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2022-12-31 01:11:29 -0300

    Criação rota listar usuários

--- routes/ProdutoRoutes.js
+++ routes/ProdutoRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const ProdutoController = require('../controllers/ProdutoController');
 
+router.get('', ProdutoController.find);
 router.post('/new', ProdutoController.create);
+
 
 module.exports = router;
