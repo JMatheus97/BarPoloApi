b573ab8b6f8eadf09f0f41bcb45d35c92405eb51
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2023-01-02 23:19:21 -0300

    Exclusão routes de Listar Usuários

--- routes/UsuarioRoutes.js
+++ routes/UsuarioRoutes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const UsuarioController= require('../controllers/UsuarioController');
 
-router.get('', UsuarioController.find);
 router.post("/new", UsuarioController.create);
 
 module.exports = router;
