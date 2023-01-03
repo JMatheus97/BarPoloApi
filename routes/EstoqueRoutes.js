ac34666bc221cb058bdf512cddb07439d62e1f07
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2023-01-02 23:18:46 -0300

    Criação Routes Estoque

--- routes/EstoqueRoutes.js
+++ routes/EstoqueRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const EstoqueController = require('../controllers/EstoqueController');
 
+router.get('', EstoqueController.find);
 router.post('/new', EstoqueController.create);
 
 module.exports = router;
