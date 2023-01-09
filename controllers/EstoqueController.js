5d6e64a61eb6175a80b6383222b2a467c193f164
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2023-01-09 20:38:28 -0300

    Criação método editar Estoque

--- controllers/EstoqueController.js
+++ controllers/EstoqueController.js
@@ -3,12 +3,26 @@ const Produto = require('../models/Produto');
 
 module.exports = class EstoqueController {
     static async create(req, res){
-        const { produto, quantidade, tipo} = req.body;
-        let produtoExists;
-        try {
-             produtoExists = await Produto.findById({ _id: produto});
-        }catch(error){
-            return res.status(422).json({ message: "Produto não encontrado", error})
+        const { produto, quantidade, lote, tipo} = req.body;
+
+        if(!produto){
+            return res.status(422).json({ message: "Informe o produto !"});
+        }else{
+            const produtoExists = await Produto.findById({ _id: produto});
+
+            if(!produtoExists){
+                return res.status(422).json({ message: "O produto informado não foi encontrado !"});
+            } 
+
+            if(!lote){
+                return res.status(422).json({ message: "Informe o lote " })
+            }
+
+            const loteExists = await Estoque.findOne({ lote });
+
+             if(loteExists){
+                return res.status(422).json({ message: "O lote já  existe, favor informar outro !"});
+            } 
         }
         
         if(!quantidade){
@@ -20,17 +34,21 @@ module.exports = class EstoqueController {
         }
 
 
+
+
         const estoque = Estoque({
             produto,
             quantidade,
+            lote,
             tipo,
     });
 
         try {
-            const resulEstoque = await estoque.save().populate("produto");
-            return res.status(200).json({ message: "Estoque registrado com sucesso !", resulEstoque });
+            const resulEstoque = await estoque.save()
+            const estoques = await Estoque.findById({ _id: resulEstoque._id}).populate("produto");
+            return res.status(200).json({ message: "Estoque registrado com sucesso !", estoques });
         }catch(error){
-            return res.status(400).json({ message: "Não foi possível registar o estoque !", error });
+            return res.status(400).json({ message: "Não foi possível registar o estoque !" });
         }
     }
 
@@ -40,6 +58,52 @@ module.exports = class EstoqueController {
             return res.status(200).json({ estoques});
         }catch(error){
             return res.status(400).json({ message: "Não foi possível listar estoque !"});
+        }
+    }
+
+    static async edit(req, res){
+        const id = req.params.id;
+
+        const { produto, quantidade, lote, tipo } = req.body;
+
+        const estoque = await Estoque.findById({ _id: id});
+
+        if(produto){
+            const produtoExists = await Produto.findById({ _id: produto});
+
+            if(!produtoExists){
+                return res.status(422).json({ message: "O produto informado não foi encontrado !"});
+            } else {
+                estoque.produto = produto;
+            }
+        }
+
+        if(quantidade){
+            estoque.quantidade = quantidade;
+        }
+
+        if(tipo){
+            estoque.tipo = tipo;
+        }
+
+        if(lote){
+            if(lote !== estoque){
+                const loteExists = await Estoque.findOne({ lote });
+
+                if(loteExists){
+                   return res.status(422).json({ message: "O lote já  existe, favor informar outro !"});
+               } else{
+                estoque.lote = lote;
+               }
+            }
+        }
+
+        try{
+            const estoqueEdit = await Estoque.findByIdAndUpdate({ _id: estoque._id}, {$set: estoque}, {new: true});
+            const resutlEstoque = await Estoque.findById({ _id: estoqueEdit._id}).populate("produto");
+            return res.status(200).json({ message: "Estoque editado com sucesso", resutlEstoque});
+        }catch(error){
+            return res.status(400).json({ message: "Erro ao editar o estoque !"});
         }
     }
 }
