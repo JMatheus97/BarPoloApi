87e48ec98825b6ee115dda265c756d8b166a4451
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2023-01-09 20:38:18 -0300

    Criação método editar Mesa

--- controllers/MesaController.js
+++ controllers/MesaController.js
@@ -2,10 +2,17 @@ const Mesa = require('../models/Mesa');
 
 module.exports = class MesaController {
     static async create(req, res) {
-        const { numeroMesa, quantidadeCliente } = req.body;
+        const { numeroMesa, quantidadeCliente, status } = req.body;
 
+        
         if(!numeroMesa) {
             return  res.status(422).json({ mesage: " Informe o numero da mesa!"});
+        }
+        
+        const consultMesaExits  = await Mesa.findOne({ numeroMesa: numeroMesa});
+
+        if(consultMesaExits){
+            return res.status(422).json({ message: "O numero de mesa já está sendo usando !"})
         }
 
         if(!quantidadeCliente){
@@ -14,7 +21,8 @@ module.exports = class MesaController {
 
         const mesa = await Mesa({ 
             numeroMesa,
-            quantidadeCliente
+            quantidadeCliente,
+            status
         });
         try{ 
             const resultMesa = await mesa.save();
@@ -32,4 +40,41 @@ module.exports = class MesaController {
             return res.status(400).json({ message: "Não foi possível listar mesas !"});
         }
     }
-}
+
+    static async edit(req, res){
+        const id = req.params.id;
+
+        const { numeroMesa, quantidadeCliente, status }  = req.body;
+
+        const mesa = await Mesa.findById({ _id: id});
+
+        if(!mesa){
+            return res.status(422).json({ message: "Não foi possível encontrar a mesa !"});
+        }
+
+        const consultMesaExits  = await Mesa.findOne({ numeroMesa: numeroMesa});
+
+        if(consultMesaExits){
+            return res.status(422).json({ message: "O numero de mesa já está sendo usando !"})
+        }
+
+        if(numeroMesa){
+            mesa.numeroMesa = numeroMesa;
+        }
+
+        if(quantidadeCliente) {
+            mesa.quantidadeCliente = quantidadeCliente;
+        }
+
+        if(status){
+            mesa.status = status;
+        }
+
+        try{    
+            const  mesaEdit = await Mesa.findByIdAndUpdate({ _id: mesa.id}, { $set: mesa }, { new: true });
+            return res.status(200).json({ mesaEdit });
+        }catch(error){
+            return res.status(400).json({ message: "Não foi possível editar a mesa"});
+        }
+    }
+} 
