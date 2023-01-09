d853ab0c29d3b9ce2207696393a42836b8a381f6
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2023-01-09 20:37:06 -0300

    Criação rota editar produto

--- routes/ProdutoRoutes.js
+++ routes/ProdutoRoutes.js
@@ -3,6 +3,7 @@ const ProdutoController = require('../controllers/ProdutoController');
 
 router.get('', ProdutoController.find);
 router.post('/new', ProdutoController.create);
+router.post('/:id', ProdutoController.edit);
 
 
 module.exports = router;
