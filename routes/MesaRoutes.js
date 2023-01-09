5d8d7bf689ab03af43b21e6ef59751030cd481be
João Matheus <39351502+JMatheus97@users.noreply.github.com>
2023-01-09 20:37:16 -0300

    Criação rota editar mesa

--- routes/MesaRoutes.js
+++ routes/MesaRoutes.js
@@ -3,5 +3,6 @@ const MesaController = require('../controllers/MesaController');
 
 router.get('', MesaController.find);
 router.post('/new', MesaController.create);
+router.post('/:id',  MesaController.edit);
 
 module.exports  = router;
